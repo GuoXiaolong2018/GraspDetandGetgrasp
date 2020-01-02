4c2284ad8544ca164a0e4d9b665483de75049a6d
soldatJiang <1516981776@qq.com>
2020-01-02 21:20:01 +0800

    fix some bugs

---
+++ data/generate_mask.py
@@ -0,0 +1,5 @@
+import numpy as np 
+import scipy.misc   
+mask = np.zeros((480,640))
+mask[60:400,100:600]=1
+scipy.misc.imsave('data/mask.jpg', mask)
--- scripts/grasp_det.py
+++ scripts/grasp_det.py
@@ -40,19 +40,27 @@ DATASETS= {'pascal_voc': ('voc_2007_trainval',),'pascal_voc_0712': ('voc_2007_tr
 
 class DetectionManager:
     def __init__(self):
+        self.ROOT_DIR = '/home/js/Documents/catkin_ws/src/grasp_detection'
         self.rgd_topic = "/rgd_image"
-        self.rgd_pub = rospy.Publisher(self.rgd_topic, Image, queue_size=1)
-        self.depth_pub = rospy.Publisher("/depth_processed", Image, queue_size=1)
-        self.result_pub = rospy.Publisher("/result_detection", Image, queue_size=1)
+        self.rgd_pub = rospy.Publisher(self.rgd_topic, Image, queue_size=10)
+        self.depth_pub = rospy.Publisher("/depth_processed", Image, queue_size=10)
+        self.result_pub = rospy.Publisher("/result_detection", Image, queue_size=10)
         self.image_topic = rospy.get_param("~image_topic", "/camera/color/image_raw")
         self.image_sub = rospy.Subscriber(self.image_topic, Image, self.imageCallback)
         self.depth_topic = rospy.get_param("~depth_topic","/camera/aligned_depth_to_color/image_raw")
         self.depth_sub = rospy.Subscriber(self.depth_topic, Image, self.depthCallback)
+        #self.detect_server = rospy.Service()
+        self.grasp_center = [0., 0.]
+        self.grasp_angle = 0.
         self.rgb = None
         self.depth = None
         self.depthr_ = None # Re-ranged depth
         self.rgd = None
         self.cvb = CvBridge()
+        self.mask = cv2.imread(os.path.join(self.ROOT_DIR,'data/mask.jpg'),0)
+        self.det_viz = None
+        #self.mask = self.mask[:,:,np.newaxis]
+        #self.mask = np.stack([self.mask, self,mask, self.mask], axis=2)
 
         self.cnt = 0
 
@@ -83,7 +91,13 @@ class DetectionManager:
         self.rgb = np.frombuffer(data.data, dtype=np.uint8).reshape(data.height, data.width, -1)
         self.rgd = self.rgb.copy()
         if self.depthr_ is not None:
+            try:
+                self.depth_pub.publish(self.cvb.cv2_to_imgmsg(self.depthr_, "mono8"))
+            except CvBridgeError as e:
+                print(e)
+
             self.rgd[:,:,2] = np.squeeze(self.depthr_)
+            self.rgd = cv2.bitwise_and(self.rgd, self.rgd, mask=self.mask)
 
             try:
                 self.rgd_pub.publish(self.cvb.cv2_to_imgmsg(self.rgd, "rgb8"))
@@ -91,14 +105,24 @@ class DetectionManager:
                 print(e)
             
             self.cnt += 1
-            rospy.loginfo("{}".format(self.cnt))
+            #rospy.loginfo("{}".format(self.cnt))
             if self.cnt == 100:
                 self.cnt = 0
                 self.detect(self.sess, self.net, self.rgd)
+                rospy.loginfo("Grasp center:({},{})".format(self.grasp_center[0], self.grasp_center[1]))
+                rospy.loginfo("Grasp angle:{}".format(self.grasp_angle))
+
+                try:
+                    self.result_pub.publish(self.cvb.cv2_to_imgmsg(self.det_viz, "rgb8"))
+                except CvBridgeError as e:
+                    print(e)
 
     def depthCallback(self, data):
         self.depth = np.frombuffer(data.data, dtype=np.uint16).reshape(data.height, data.width, -1)
-        self.depthr_ = np.uint8((self.depth.astype(np.float32)-500.)/(1200.-500.)*255) # Re-range depth value to 0~255
+        temp = (self.depth.astype(np.float32)-440.)/(510.-440.) # Re-range depth value to 0~255
+        temp[np.where(temp>1.0)] = 0.
+        #temp[np.where(temp<0.0)] = 0.
+        self.depthr_ = np.uint8(temp*255.)
 
         try:
             self.depth_pub.publish(self.cvb.cv2_to_imgmsg(self.depthr_, "mono8"))
@@ -116,11 +140,14 @@ class DetectionManager:
         scores, boxes = im_detect(sess, net, im)
         timer.toc()
 
+        self.det_viz = self.rgb.copy()
+
         rospy.loginfo('Detection took {:.3f}s for {:d} object proposals'.format(timer.total_time, boxes.shape[0]))
 
         # Visualize detections for each class
         CONF_THRESH = 0.1	
         NMS_THRESH = 0.3
+        max_score = 0.0
         for cls_ind, cls in enumerate(CLASSES[1:]):
             cls_ind += 1 # because we skipped background
             cls_boxes = boxes[:, 4*cls_ind:4*(cls_ind + 1)]
@@ -129,14 +156,16 @@ class DetectionManager:
                             cls_scores[:, np.newaxis])).astype(np.float32)
             keep = nms(dets, NMS_THRESH)
             dets = dets[keep, :]
-            #rospy.loginfo(dets)
-            self.vis_detections(im, cls, dets, thresh=CONF_THRESH)
-            #vis_detections(ax, image_name, im, cls, dets, thresh=CONF_THRESH)
-            #tmp = max(cls_scores)
+            self.vis_detections(im, cls, dets)
+            if dets[0,4] > max_score:
+                max_score = dets[0,4]
+                self.grasp_center[0] = (dets[0,0] + dets[0,2])/2
+                self.grasp_center[1] = (dets[0,1] + dets[0,3])/2
+                tmp = int(cls[6:])
+                self.grasp_angle = 9*(tmp-1)
 
     def vis_detections(self, im, class_name, dets, thresh=0.5):
         """Draw detected bounding boxes."""
-        imOut = self.rgb.copy()
         inds = np.where(dets[:, -1] >= thresh)[0]
         if len(inds) == 0:
             return
@@ -145,13 +174,6 @@ class DetectionManager:
             bbox = dets[i, :4]
             score = dets[i, -1]
 
-            #ax.add_patch(
-            #    plt.Rectangle((bbox[0], bbox[1]),
-            #                  bbox[2] - bbox[0],
-            #                  bbox[3] - bbox[1], fill=False,
-            #                  edgecolor='red', linewidth=3.5)
-            #    )
-
             # plot rotated rectangles
             pts = ar([[bbox[0],bbox[1]], [bbox[2], bbox[1]], [bbox[2], bbox[3]], [bbox[0], bbox[3]]])
             cnt = ar([(bbox[0] + bbox[2])/2, (bbox[1] + bbox[3])/2])
@@ -159,12 +181,35 @@ class DetectionManager:
             r_bbox = self.Rotate2D(pts, cnt, -pi/2-pi/20*(angle-1))
             r_bbox = np.array(r_bbox).astype(np.int32)
             #pred_label_polygon = Polygon([(r_bbox[0,0],r_bbox[0,1]), (r_bbox[0,0],r_bbox[0,1]), (r_bbox[2,0], r_bbox[2,1]), (r_bbox[3,0], r_bbox[3,1])])
-            cv2.line(imOut, (r_bbox[0,0],r_bbox[0,1]), (r_bbox[1,0], r_bbox[1,1]), (255,0,0))
-            cv2.line(imOut, (r_bbox[1,0],r_bbox[1,1]), (r_bbox[2,0], r_bbox[2,1]), (0,0,255))
-            cv2.line(imOut, (r_bbox[2,0],r_bbox[2,1]), (r_bbox[3,0], r_bbox[3,1]), (255,0,0))
-            cv2.line(imOut, (r_bbox[3,0],r_bbox[3,1]), (r_bbox[0,0], r_bbox[0,1]), (0,0,255))
+            cv2.line(self.det_viz, (r_bbox[0,0],r_bbox[0,1]), (r_bbox[1,0], r_bbox[1,1]), (0,0,0))
+            cv2.line(self.det_viz, (r_bbox[1,0],r_bbox[1,1]), (r_bbox[2,0], r_bbox[2,1]), (255,0,0))
+            cv2.line(self.det_viz, (r_bbox[2,0],r_bbox[2,1]), (r_bbox[3,0], r_bbox[3,1]), (0,0,0))
+            cv2.line(self.det_viz, (r_bbox[3,0],r_bbox[3,1]), (r_bbox[0,0], r_bbox[0,1]), (255,0,0))
 
-            self.result_pub.publish(self.cvb.cv2_to_imgmsg(imOut, "rgb8"))
+    def vis_detection_max(self, im, class_name, dets):
+        """Draw detected bounding boxes."""
+        imOut = self.rgb.copy()
+        ind = -1
+        ind = np.argmax(dets[:, -1])
+        if ind == -1:
+            return
+
+        bbox = dets[ind, :4]
+        score = dets[ind, -1]
+
+        # plot rotated rectangles
+        pts = ar([[bbox[0],bbox[1]], [bbox[2], bbox[1]], [bbox[2], bbox[3]], [bbox[0], bbox[3]]])
+        cnt = ar([(bbox[0] + bbox[2])/2, (bbox[1] + bbox[3])/2])
+        angle = int(class_name[6:])
+        r_bbox = self.Rotate2D(pts, cnt, -pi/2-pi/20*(angle-1))
+        r_bbox = np.array(r_bbox).astype(np.int32)
+        #pred_label_polygon = Polygon([(r_bbox[0,0],r_bbox[0,1]), (r_bbox[0,0],r_bbox[0,1]), (r_bbox[2,0], r_bbox[2,1]), (r_bbox[3,0], r_bbox[3,1])])
+        cv2.line(imOut, (r_bbox[0,0],r_bbox[0,1]), (r_bbox[1,0], r_bbox[1,1]), (0,0,0))
+        cv2.line(imOut, (r_bbox[1,0],r_bbox[1,1]), (r_bbox[2,0], r_bbox[2,1]), (255,0,0))
+        cv2.line(imOut, (r_bbox[2,0],r_bbox[2,1]), (r_bbox[3,0], r_bbox[3,1]), (0,0,0))
+        cv2.line(imOut, (r_bbox[3,0],r_bbox[3,1]), (r_bbox[0,0], r_bbox[0,1]), (255,0,0))
+
+        self.result_pub.publish(self.cvb.cv2_to_imgmsg(imOut, "rgb8"))
 
 if __name__ == "__main__":
     rospy.init_node("get_image_node")
