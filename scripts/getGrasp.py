b89f119afcc06403a423ba896635e074c98dd323
soldatJiang <1516981776@qq.com>
2020-01-14 12:01:37 +0800

    reconstruct get grasp code

--- scripts/getGrasp.py
+++ scripts/getGrasp.py
@@ -10,6 +10,20 @@ import json
 
 #export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/gxl/Downloads/Dobot-Magician-ROS-Demo/dobot_ws/src/dobot/src/DobotDll_x64
 
+def load_transform(base_path, filename):
+    s = None
+    R = None
+    T = None
+    with open(base_path+filename,'r') as f:
+            p = json.load(f)
+            s = np.array(p["s"])
+            R = np.array(p["R"])
+            T = np.array(p["T"])
+    return s,R,T
+
+def goto_point(api, dst, mode=dType.PTPMode.PTPMOVJXYZMode):
+    dType.SetPTPCmd(api, mode, dst[0], dst[1], dst[2], dst[3], isQueued=1)
+
 def grasp_client():
     rospy.init_node('grasp_client')
     rospy.wait_for_service("/grasp_request")
@@ -40,8 +54,19 @@ if __name__ == "__main__":
     #Connect Dobot
     state = dType.ConnectDobot(api, "", 115200)[0]
     print("Connect status:",CON_STR[state])
-    if (state == dType.DobotConnect.DobotConnect_NoError):
 
+    # 箱子坐标
+    homex = 0
+    homey = -200
+    homez = 40
+    homer = 0
+
+    # 相机内参矩阵的逆矩阵,用于将图像坐标转换为相机坐标
+    Minv = np.array([[0.0016221626347814378, 0.0, -0.5337791036253712], [0.0, 0.0016235716628296255, -0.39760458236866114], [0.0, 0.0, 1.0]])
+    s,R,T = load_transform('/home/js/Documents/catkin_ws/src/grasp_detection/scripts', 'calibration_results.json')
+    T = T.reshape(3,1)
+
+    if (state == dType.DobotConnect.DobotConnect_NoError):
         #Clean Command Queued
         dType.SetQueuedCmdClear(api)
         
@@ -54,84 +79,61 @@ if __name__ == "__main__":
 
         dType.SetQueuedCmdStartExec(api)
 
-        #dType.SetHOMECmd(api, temp=1, isQueued=0)			#temp是无效值
-        #dType.dSleep(ms=5000)							#延时5s操作
-
-        homex = 0;homey = -200;homez = 40;homer = 0;
-        xyzvel = 200;xyzacc = 200;rvel = 200;racc = 200;
-
         x3=homex;y3=homey;z3=homez;r3=0;#箱子上方
         x4=x3;y4=y3;z4=-28;r4=0;#箱子里
 
-        #dType.SetHOMEParams(api,  homex,  homey,  homez,  homer,  isQueued=1)#设置回零参数#
-        #dType.SetHOMECmd(api,1,1)
-
-        #Start to Execute Command Queued
-
-        #从家出发#移动到home处#
+        #移动到home处,将机械臂移除视野
         dType.SetPTPCmd(api, dType.PTPMode.PTPMOVJXYZMode, homex, homey, homez, homer, isQueued=1)
         dType.dSleep(ms=2000)
 
-        a = grasp_client()
-        print("cx = {} ; cy = {} ; angle = {} ; a.depth = {}\n".format(a.cx,a.cy,a.angle,a.depth))
+        grasp = grasp_client()
+        print("cx = {} ; cy = {} ; angle = {} ; a.depth = {}\n".format(grasp.cx,grasp.cy,grasp.angle,grasp.depth))
 
-        if a.depth == 0.0:
-            a.depth = 460.
+        if grasp.depth == 0.0:
+            grasp.depth = 460.
 
-        T = None;s = None;R = None;
-        Minv = np.array([[0.0016221626347814378, 0.0, -0.5337791036253712], [0.0, 0.0016235716628296255, -0.39760458236866114], [0.0, 0.0, 1.0]])
-        tmp = np.zeros((3,1),dtype = np.float32);tmp[0,0] = a.cx;tmp[1,0] = a.cy;tmp[2,0] = 1.0;
-        C = a.depth * np.dot(Minv,tmp)
-        print("##c :",C)
-        
-        with open('/home/js/Documents/catkin_ws/src/grasp_detection/scripts/calibration_results.json','r') as f:
-            p = json.load(f)
-            #print(p)
-            T = np.array(p["T"]);s = np.array(p["s"]);R = np.array(p["R"])
-        print("##T = {} \nR = {} \ns = {} \n".format(T,R,s))
-        T = T.reshape(3,1)
-        print("##t.reshape :",T)
+        #图像坐标系下坐标,齐次坐标
+        image = np.zeros((3,1),dtype = np.float32)
+        image[0,0] = grasp.cx
+        image[1,0] = grasp.cy
+        image[2,0] = 1.0
 
-        TARGET = s * np.dot(R,C) + T
-        print("##target :",TARGET)
+        camera = grasp.depth * np.dot(Minv,image)
+        print("## Camera coordinate :",camera)
 
-        x1=TARGET[0,0];y1=TARGET[1,0];z1=10;r1=check_angle(a.angle);#目标上方#待定 #a.angle+90 #-237~63
-        x2=x1;y2=y1;z2=-31;r2=r1;#目标处#待定 
+        target = s * np.dot(R,camera) + T
+        print("##target :",target)
+
+        #目标上方#待定 #a.angle+90 #-237~63
+        p1 = target[0,0], target[1,0], 10, check_angle(grasp.angle)
+        #目标处#待定
+        p2 = p1[0], p1[1], -32, p1[3]
+        p3 = homex, homey, homez, homer
+        p4 = p3[0], p3[1], -28, p3[3]
 
         #移动到目标上方x1
-        dType.SetPTPCoordinateParams(api, xyzvel, xyzacc, rvel,  racc,  isQueued=1)
-        dType.SetPTPCmd(api, dType.PTPMode.PTPMOVJXYZMode, x1, y1, z1, r1, isQueued=1)
-        #dType.dSleep(ms=2000) 
+        goto_point(api, p1)
         dType.SetEndEffectorGripper(api, enableCtrl=1,  on=0, isQueued=1)#爪子张开#
         dType.SetWAITCmd(api, 1, isQueued=1)
 
         #移动到目标处
         #移动到x2处#
-        dType.SetPTPCmd(api, dType.PTPMode.PTPMOVJXYZMode, x2, y2, z2, r2, isQueued=1)
-        #dType.dSleep(ms=2000) 
+        goto_point(api, p2)
         dType.SetEndEffectorGripper(api, enableCtrl=1,  on=1, isQueued=1)#爪子夹取#
-        dType.SetWAITCmd(api, 1, isQueued=1)
-        #dType.dSleep(ms=2000) 
-
-        #移动到目标上方x1
-        #移动到x1处#
-        dType.SetPTPCmd(api, dType.PTPMode.PTPMOVJXYZMode, x1, y1, z1, r1, isQueued=1)
-        #dType.dSleep(ms=2000)
+        dType.SetWAITCmd(api, 2, isQueued=1)
 
         #移动到箱子上方
         #移动到x3处#
-        dType.SetPTPCmd(api, dType.PTPMode.PTPMOVJXYZMode, x3, y3, z3, r3, isQueued=1)
-        #dType.dSleep(ms=2000)
+        goto_point(api, p3)
 
         #移动到箱子里x4处#
-        dType.SetPTPCmd(api, dType.PTPMode.PTPMOVJXYZMode, x4, y4, z4, r4, isQueued=1)
+        goto_point(api, p4)
         #dType.dSleep(ms=2000)
         dType.SetEndEffectorGripper(api, enableCtrl=1,  on=0, isQueued=1)#爪子张开#
-        dType.SetWAITCmd(api, 1, isQueued=1)
+        dType.SetWAITCmd(api, 2, isQueued=1)
 
         #移动到箱子上方#移动到x3处#
-        dType.SetPTPCmd(api, dType.PTPMode.PTPMOVJXYZMode, x3, y3, z3, r3, isQueued=1)
-        #dType.dSleep(ms=2000)
+        goto_point(api, p3)
         dType.SetEndEffectorGripper(api, enableCtrl=1,  on=1, isQueued=1)#爪子闭合#
         dType.SetWAITCmd(api, 1, isQueued=1)
 
@@ -139,9 +141,6 @@ if __name__ == "__main__":
 
         #归零
         dType.SetHOMECmd(api,1,isQueued=1)   
-
-    #Clean Command Queued
-    #dType.SetQueuedCmdClear(api)
     #Disconnect Dobot
     dType.DisconnectDobot(api)
 
